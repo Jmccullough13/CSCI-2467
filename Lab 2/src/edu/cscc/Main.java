a9418a4437e5040e4f6c90a25f39ae62cd117b77
Jmccullough13 <124702934+Jmccullough13@users.noreply.github.com>
2023-02-07 10:55:22 -0500

    Update Main.java
    Corrected code

--- Lab 2/src/edu/cscc/Main.java
+++ Lab 2/src/edu/cscc/Main.java
@@ -12,39 +12,34 @@ public class Main {
         speed = input.nextLong();
         if (speed < 0) {
             System.out.print("Invalid input. Wind speeds cannot be negative.");
-        } else {
-            if (speed < 39) {
-                classification = ("Not in scale");
-                System.out.print("Classification: " + classification);
-            } else {
-                if (speed < 74) {
-                    classification = ("Tropical storm");
-                    System.out.print("Classification: " + classification);
-                } else {
-                    if (speed < 96) {
-                        classification = ("Category One Hurricane");
-                        System.out.print("Classification: " + classification);
-                    } else {
-                        if (speed < 111) {
-                            classification = ("Category Two Hurricane");
-                            System.out.print("Classification: " + classification);
-                        } else {
-                            if (speed < 130) {
-                                classification = ("Category Three Hurricane");
-                                System.out.print("Classification: " + classification);
-                            } else {
-                                if (speed < 157) {
-                                    classification = ("Category Four Hurricane");
-                                    System.out.print("Classification: " + classification);
-                                } else {
-                                    classification = ("Category Five Hurricane");
-                                    System.out.print("Classification: " + classification);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+        } 
+        else if (speed < 39) {
+            classification = ("Not in scale");
+            System.out.print("Classification: " + classification);
+        } 
+        else if (speed < 74) {
+            classification = ("Tropical storm");
+            System.out.print("Classification: " + classification);
+        }
+        else if (speed < 96) {
+             classification = ("Category One Hurricane");
+            System.out.print("Classification: " + classification);
+        } 
+        else if (speed < 111) {
+            classification = ("Category Two Hurricane");
+            System.out.print("Classification: " + classification);
+        }
+        else if (speed < 130) {
+            classification = ("Category Three Hurricane");
+            System.out.print("Classification: " + classification);
+        }
+        else if (speed < 157) {
+            classification = ("Category Four Hurricane");
+            System.out.print("Classification: " + classification);
+        }
+        else {
+        classification = ("Category Five Hurricane");
+        System.out.print("Classification: " + classification);
         }
     }
 }
